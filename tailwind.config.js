9cdce294624f4ead20a2d86ee22f13cc38bf05a5
JunHo Kim <84065408+CONT-Mood@users.noreply.github.com>
2025-11-21 18:33:50 +0900

    chore: 프로젝트 초기 설정

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B2621',
+        secondary: '#5A4A42',
+        accent: '#4A90E2',
+      },
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
