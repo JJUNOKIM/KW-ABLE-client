47b421c24fdd1207bb7079ff40bd3d7742e2152c
JunHo Kim <84065408+CONT-Mood@users.noreply.github.com>
2025-11-26 18:41:16 +0900

    feat: 경로 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MapPage from './pages/MapPage'
 import RouteSelectionPage from './pages/RouteSelectionPage'
+import RouteResultPage from './pages/RouteResultPage'
+import RouteGuidancePage from './pages/RouteGuidancePage'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<MapPage />} />
           <Route path="/route" element={<RouteSelectionPage />} />
+          <Route path="/route-result" element={<RouteResultPage />} />
+          <Route path="/route-guidance" element={<RouteGuidancePage />} />
         </Routes>
       </Router>
     </div>
